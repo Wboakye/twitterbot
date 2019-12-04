24d0e432be781b6d35e25c6c19b9785738819aba
William Boakye <wboakye@gmail.com>
2019-12-03 22:56:40 -0500

    modified env

--- bots/config.py
+++ bots/config.py
@@ -7,13 +7,12 @@ logger = logging.getLogger()
 
 
 def create_api():
-    s3 = S3Connection(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"],
-                      os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
-
-    consumer_key = s3[0]
-    consumer_secret = s3[1]
-    access_token = s3[2]
-    access_token_secret = s3[3]
+    s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+    
+    consumer_key = s3.get_bucket("CONSUMER_KEY", validate=False, headers=False) 
+    consumer_secret = s3.get_bucket("CONSUMER_SECRET", validate=False, headers=False)
+    access_token = s3.get_bucket("ACCESS_TOKEN", validate=False, headers=False)
+    access_token_secret = s3.get_bucket("ACCESS_TOKEN_SECRET", validate=False, headers=False)
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
