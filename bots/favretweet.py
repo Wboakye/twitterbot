dafa7bab54a4ebe21ac18d05b030ed4264639e22
William Boakye <wboakye@gmail.com>
2019-12-11 11:17:51 -0500

    make it better

--- bots/favretweet.py
+++ bots/favretweet.py
@@ -45,7 +45,7 @@ class FavRetweetListener(tweepy.StreamListener):
                 tweet.retweet()
             except Exception as e:
                 print("Error on retweet")
-        time.sleep(180)
+        time.sleep(90)
 
     def on_error(self, status):
         print(status)
