e5b36eabe737ec976aa25d4659eeec7d28868375
William Boakye <wboakye@gmail.com>
2019-12-11 21:50:53 -0500

    make it better

--- bots/favretweet.py
+++ bots/favretweet.py
@@ -73,9 +73,8 @@ class FavRetweetListener(tweepy.StreamListener):
             time.sleep(1)
 
     def screenshot(self):
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        current_date = datetime.today()
+        current_time = datetime.now().strftime("%H:%M:%S")
+        current_date = datetime.today().strftime('%y%m%d')
 
         self.db.posts.insert_one({"date": current_date, "time": current_time,
                                   "tweetCount": self.me.statuses_count, "followerCount": self.me.followers_count})
